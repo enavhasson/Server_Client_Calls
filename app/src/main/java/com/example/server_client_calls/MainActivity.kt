50b4cd0c3101988c42b1606c8f0ef73a514048f0
enavhasson <enav.hasson@cs.huji.ac.il>
2020-06-26 18:23:08 +0300

    first commit

---
+++ app/src/main/java/com/example/server_client_calls/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.server_client_calls
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
