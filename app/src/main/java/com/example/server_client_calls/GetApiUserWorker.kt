31b79380d5a57eefa7ba88ca0ac0a3a3ef2bf26b
enavhasson <enav.hasson@cs.huji.ac.il>
2020-06-30 15:47:18 +0300

    update

--- app/src/main/java/com/example/server_client_calls/GetApiUserWorker.kt
+++ app/src/main/java/com/example/server_client_calls/GetApiUserWorker.kt
@@ -83,4 +83,30 @@ class GetApiUserUpdatePrettyWorker(context: Context, workerParams: WorkerParamet
     }
 }
 
+class UpdateImgUrlWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
+
+    override fun doWork(): Result {
+        val input = "token " + inputData.getString(TOKEN_KEY)
+        val request =UserService.SetUserImgUrlRequest()
+        request.image_url= inputData.getString(IMG_URL_KEY)
+
+        val callToSlash = ServerHolder.getInstance()
+            .server
+            .updateUserImageUrl(input,request)
+
+        val response = callToSlash?.execute()
+        if (response != null) {
+            Log.d("UpdateImgUrl", "got response: ${response.code()}")
+            if (response.code() != 200 || !response.isSuccessful) {
+                return Result.failure()
+            }
+        }
+        val userResult: UserService.UserResponse = response?.body() ?: return Result.failure()
+        Log.d("UpdateImgUrlWorker", "got response with result: ${userResult.data}")
+        val userInfo = Gson().toJson(userResult)
+        return Result.success(Data.Builder().putString(USER_INFO_KEY, userInfo).build())
+    }
+}
+
 
--- app/src/main/java/com/example/server_client_calls/MainActivity.kt
+++ app/src/main/java/com/example/server_client_calls/MainActivity.kt
@@ -6,20 +6,16 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.core.app.ActivityCompat
-import androidx.work.OneTimeWorkRequest
-
 import android.util.Log
+import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
 import androidx.work.*
 import com.bumptech.glide.Glide
 import com.google.gson.Gson
-import com.squareup.picasso.MemoryPolicy
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val TOKEN_KEY = "token"
@@ -38,6 +34,16 @@ class MainActivity : AppCompatActivity() {
     private val PRETTY_NAME_STR = "welcome again, "
     private val PRETTY_NAME_STR_NULL = "welcome "
     private val USERNAME_SP = "username"
+
+    private val CRAB_IMG_URL = "/images/crab.png"
+    private val UNICORN_IMG_URL = "/images/unicorn.png"
+    private val ALIEN_IMG_URL = "/images/alien.png"
+    private val ROBOT_IMG_URL = "/images/robot.png"
+    private val OCTOPUS_IMG_URL = "/images/octopus.png"
+    private val FROG_IMG_URL = "/images/frog.png"
+    private val IMAGES = arrayOf("/images/crab.png", "/images/unicorn.png", "/images/alien.png",
+        "/images/robot.png", "/images/octopus.png", "/images/frog.png")
+
     private lateinit var sp: SharedPreferences
     private var token: String? = null
 
@@ -58,8 +64,8 @@ class MainActivity : AppCompatActivity() {
         token = sp.getString(TOKEN_KEY, null)
         init()
         initButton()
-
         if (token != null) {
+            initImages()
             removeInsertUsernameView()
             getUserInfo(this.token!!)
         }
@@ -77,7 +83,8 @@ class MainActivity : AppCompatActivity() {
         insertButton = findViewById(R.id.insert_button)
         insertPrettyButton = findViewById(R.id.insertPrettyNameButton)
 
-        profileImg=findViewById(R.id.profileImageView)
+        profileImg = findViewById(R.id.profileImageView)
+        profileImg.visibility = View.INVISIBLE
     }
 
     private fun initView() {
@@ -85,11 +92,43 @@ class MainActivity : AppCompatActivity() {
         prettyNameTextView.visibility = View.INVISIBLE
     }
 
-    private fun removeInsertUsernameView(){
-        insertButton.visibility=View.INVISIBLE
-        usernameTextView.visibility=View.INVISIBLE
+    fun showImagesUI(){
+        showImgUI(ServerHolder.BASE_URL+ALIEN_IMG_URL,findViewById(R.id.alien_image))
+        showImgUI(ServerHolder.BASE_URL+CRAB_IMG_URL,findViewById(R.id.crab_image))
+        showImgUI(ServerHolder.BASE_URL+FROG_IMG_URL,findViewById(R.id.frog_image))
+        showImgUI(ServerHolder.BASE_URL+OCTOPUS_IMG_URL,findViewById(R.id.octopus_image))
+        showImgUI(ServerHolder.BASE_URL+ROBOT_IMG_URL,findViewById(R.id.robot_image))
+        showImgUI(ServerHolder.BASE_URL+UNICORN_IMG_URL,findViewById(R.id.unicorn_image))
+    }
 
-        insertPrettyButton.visibility=View.VISIBLE
+    private fun initImages() {
+        showImagesUI()
+        findViewById<View>(R.id.alien_image).setOnClickListener { view: View? ->
+            updateUserImgUrl(ALIEN_IMG_URL)
+        }
+        findViewById<View>(R.id.crab_image).setOnClickListener { view: View? ->
+            updateUserImgUrl(CRAB_IMG_URL)
+        }
+        findViewById<View>(R.id.frog_image).setOnClickListener { view: View? ->
+            updateUserImgUrl(FROG_IMG_URL)
+        }
+        findViewById<View>(R.id.octopus_image).setOnClickListener { view: View? ->
+            updateUserImgUrl(OCTOPUS_IMG_URL)
+        }
+        findViewById<View>(R.id.robot_image).setOnClickListener { view: View? ->
+            updateUserImgUrl(ROBOT_IMG_URL)
+        }
+        findViewById<View>(R.id.unicorn_image).setOnClickListener { view: View? ->
+            updateUserImgUrl(UNICORN_IMG_URL)
+        }
+    }
+
+
+    private fun removeInsertUsernameView() {
+        insertButton.visibility = View.INVISIBLE
+        usernameTextView.visibility = View.INVISIBLE
+        prettyNameTextView.visibility = View.VISIBLE
+        insertPrettyButton.visibility = View.VISIBLE
     }
 
     private fun initButton() {
@@ -97,6 +136,7 @@ class MainActivity : AppCompatActivity() {
             val username = usernameTextView.text.toString()
             if (checkValidUS(username)) {
                 getUserToken(username)
+                initImages()
             } else {
                 Toast.makeText(
                     applicationContext,
@@ -108,7 +148,7 @@ class MainActivity : AppCompatActivity() {
         insertPrettyButton.setOnClickListener(View.OnClickListener {
             showAddPrettyNameDialog(this)
         })
-        insertPrettyButton.visibility=View.INVISIBLE
+        insertPrettyButton.visibility = View.INVISIBLE
     }
 
     private fun checkValidUS(username: String): Boolean {
@@ -140,7 +180,8 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == PERMISSION_INTERNET_ID) {
             if (!grantResults.isNotEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(
-                        this, Manifest.permission.INTERNET)
+                        this, Manifest.permission.INTERNET
+                    )
                 ) {
                     // reached here? means we asked the user for this permission more than once,
                     // and they still refuse. This would be a good time to open up a dialog
@@ -194,27 +235,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("SetTextI18n")
-    private fun updatePrettyNameUI(prettyName: String?,username: String?) {
-        if (prettyName.isNullOrEmpty()) {
-            prettyNameTextView.text = PRETTY_NAME_STR_NULL +username
+    private fun updatePrettyNameUI(user: UserService.User) {
+        if (user.pretty_name.isNullOrEmpty()) {
+            prettyNameTextView.text = PRETTY_NAME_STR_NULL + user.username
         } else {
-            prettyNameTextView.text = PRETTY_NAME_STR + prettyName
+            prettyNameTextView.text = PRETTY_NAME_STR + user.pretty_name
         }
+        loadingTextView.visibility = View.INVISIBLE
+    }
 
+
+    fun updateUI(user: UserService.User) {
+        updateImgUrl(user.image_url)
+        updatePrettyNameUI(user)
+        loadingTextView.visibility = View.INVISIBLE
+        prettyNameTextView.visibility = View.VISIBLE
+        profileImg.visibility = View.VISIBLE
 
     }
 
-    fun updateUI(user: UserService.User){
-        loadingTextView.visibility = View.INVISIBLE
-        updatePrettyNameUI(user.pretty_name, user.username)
-        updateImgUrl(user.image_url)
-        profileImageView.visibility= View.VISIBLE
-        prettyNameTextView.visibility = View.VISIBLE
+    private fun showImgUI(img_url: String, intoImage: ImageView) {
+        Glide.with(this).load(img_url).into(intoImage)
     }
 
     fun updateImgUrl(img_url: String?) {
-        if (img_url!=null){
-            Glide.with(this).load(ServerHolder.BASE_URL + img_url).into(profileImg)
+        if (img_url != null) {
+            showImgUI(ServerHolder.BASE_URL + img_url, profileImg)
         }
     }
 
@@ -230,7 +276,7 @@ class MainActivity : AppCompatActivity() {
 
         WorkManager.getInstance(this).getWorkInfoByIdLiveData(workerRequest.id)
             .observe(this, Observer { workInfo ->
-                if (workInfo == null){
+                if (workInfo == null) {
                     return@Observer
                 }
 
@@ -245,6 +291,7 @@ class MainActivity : AppCompatActivity() {
                     val dataUserInfo =
                         Gson().fromJson(userInfoJson, UserService.UserResponse::class.java).data
                     updateUI(dataUserInfo)
+
                     return@Observer
                 } else {
                     // not interesting, wait until the worker will reach the state we want
@@ -270,11 +317,11 @@ class MainActivity : AppCompatActivity() {
         WorkManager.getInstance(this).getWorkInfoByIdLiveData(workerRequest.id)
             .observe(this, Observer { workInfo ->
                 if (workInfo == null) return@Observer
-
                 Log.d("ClientServerActivity", "worker has reached state ${workInfo.state}")
 
                 if (workInfo.state == WorkInfo.State.FAILED) {
                     return@Observer
+
                 } else if (workInfo.state == WorkInfo.State.SUCCEEDED) {
                     // update UI for success! :)
                     val userInfoJson = workInfo.outputData.getString(USER_INFO_KEY)
@@ -286,15 +333,51 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     // not interesting, wait until the worker will reach the state we want
                     loadingTextView.visibility = View.VISIBLE
-                    profileImageView.visibility= View.INVISIBLE
-                    prettyNameTextView.visibility= View.INVISIBLE
+                    return@Observer
+                }
+            })
+    }
+
+    private fun updateUserImgUrl(newImgUrl: String) {
+        val workerRequest = OneTimeWorkRequest.Builder(UpdateImgUrlWorker::class.java)
+            .setConstraints(
+                Constraints.Builder().setRequiredNetworkType
+                    (NetworkType.CONNECTED).build()
+            )
+            .setInputData(
+                Data.Builder().putString(TOKEN_KEY, token).putString(IMG_URL_KEY, newImgUrl)
+                    .build()
+            ).build()
+
+        WorkManager.getInstance(this).enqueue(workerRequest)
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(workerRequest.id)
+            .observe(this, Observer { workInfo ->
+                if (workInfo == null) return@Observer
+                Log.d("ClientServerActivity", "worker has reached state ${workInfo.state}")
+
+                if (workInfo.state == WorkInfo.State.FAILED) {
+                    return@Observer
+
+                } else if (workInfo.state == WorkInfo.State.SUCCEEDED) {
+                    // update UI for success! :)
+                    val userInfoJson = workInfo.outputData.getString(USER_INFO_KEY)
+                    Log.d("UpdateUserImgUrl", "userInfo : $userInfoJson")
+                    val imgUrl =
+                        Gson().fromJson(userInfoJson, UserService.UserResponse::class.java).data.image_url
+                    loadingTextView.visibility = View.INVISIBLE
+                    updateImgUrl(imgUrl)
+                    return@Observer
+                } else {
+                    // not interesting, wait until the worker will reach the state we want
+                    loadingTextView.visibility = View.VISIBLE
                     return@Observer
                 }
             })
     }
 
 
-    private fun showAddPrettyNameDialog(c: Context){
+    private fun showAddPrettyNameDialog(c: Context) {
         val inputText = EditText(c)
         val dialog = AlertDialog.Builder(c)
             .setView(inputText)
